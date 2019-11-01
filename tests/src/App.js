03cbdbc94da3ab0ce280bcbd30ddcc7dbcbf7909
Gabriel Coelho <gabriellvs60@gmail.com>
2019-11-01 13:55:24 -0300

    Mocks test
    Um pouco sobre mocks

--- tests/src/App.js
+++ tests/src/App.js
@@ -3,7 +3,7 @@ export const add = (x, y) => x + y;
 export const total = (shipping, subTotal) => {
   return '$' + add(shipping, subTotal);
 };
-
+//https://youtu.be/2d-SX8YRyis
 //export const add = (x, y) => x + y;
 
 
--- tests/src/App.test.js
+++ tests/src/App.test.js
@@ -1,11 +1,18 @@
-import { add, total } from './App';
+import { total } from './App';
+
+const add = jest.fn(() => 3);
 
 //unit test
 //it only tests one thing
 test('Add', () =>{
   expect(add(1,2)).toBe(3);
+  expect(add(1,2)).toBe(3);
+  expect(add).toHaveBeenCalledTimes(2);
+  expect(add).toHaveBeenCalledWith(1, 2);
 }); 
 
-test('total', () => {
+//integration tests
+//tests things working together
+/* test('total', () => {
   expect(total(5,20 )).toBe('$25');
-})
+}) */
